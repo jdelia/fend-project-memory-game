1df4d767cdac86a5e761b17fd7084513ba9260c1
Jackie D'Elia <jackie@jackiedelia.com>
2018-12-25 10:41:43 -0500

    Clean up code, add star update, and animation tweaks.

--- js/app.js
+++ js/app.js
@@ -9,26 +9,28 @@
  *   - add each card's HTML to the page
  */
 
-var cards;
-var timer;
-var score;
-var success;
-var starsRemoved = 0;
+//var cards;
+//var score;
+var stars = 5;
+var currentStars = 5;
 var moves = 0;
 var actualTime;
-var cardsOpen = [];
+var cardsFlipped = [];
 var cardsMatched = 0;
 var clockID = null;
 var firstClick = true;
-var timer = [0, 0, 0, 600100];
+var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
 
 const CLOCK = document.querySelector(".clock");
 const MOVES = document.querySelector(".moves");
-const STARS = document.querySelector(".stars");
+const STARSET = document.querySelectorAll(".stars li");
 const DECK = document.querySelector(".deck");
 const RESTART = document.querySelector(".restart");
+const SUCCESS = document.querySelector(".success");
+const FINAL = document.querySelector(".final-score");
+const PLAYAGAIN = document.querySelector(".play-again");
 const CARDSET = [
 	"diamond",
 	"diamond",
@@ -48,29 +50,37 @@ const CARDSET = [
 	"bomb"
 ];
 
-function generateCard(card) {
-	return `<li data-card="${card}" class="card"><div class="front"></div><div class="back"><i class="fa fa-${card}"></div></i></li>`;
-}
-
 function updateStars() {
-	// number of stars right now
-	list = STARS.querySelectorAll("li");
-
-	console.log("List", list);
-	// if (list.length > 0) {
-	// 	if (moves > 12 && moves <= 16) {
-	// 		list[list.length - 1].remove();
-	// 	}
-	// 	if (moves > 16 && moves <= 20) {
-	// 		list[list.length - 1].remove();
-	// 	}
-	// 	if (moves > 20 && moves <= 24) {
-	// 		list[list.length - 1].remove();
-	// 	}
-	// 	if (moves > 24 && moves <= 30) {
-	// 		list[list.length - 1].remove();
-	// 	}
-	// }
+	if (moves > 12 && moves <= 16) {
+		currentStars = 4;
+		if (currentStars != stars) {
+			STARSET[currentStars].classList.add("hide");
+		}
+	}
+	if (moves > 16 && moves <= 20) {
+		currentStars = 3;
+		if (currentStars != stars) {
+			STARSET[currentStars].classList.add("hide");
+		}
+	}
+	if (moves > 20 && moves <= 24) {
+		currentStars = 2;
+		if (currentStars != stars) {
+			STARSET[currentStars].classList.add("hide");
+		}
+	}
+	if (moves > 24) {
+		currentStars = 2;
+		if (currentStars != stars) {
+			STARSET[currentStars].classList.add("hide");
+		}
+	}
+	if (moves > 30) {
+		currentStars = 1;
+		if (currentStars != stars) {
+			STARSET[currentStars].classList.add("hide");
+		}
+	}
 }
 
 // Add a leading zero to single digit numbers for nicely formatted display.
@@ -81,7 +91,7 @@ function leadingZero(time) {
 	return time;
 }
 
-// Run a time with mins, secs and hundredths of secs.
+// Run a timer with mins, secs and hundredths of secs.
 function runClock() {
 	let currentTime =
 		leadingZero(timer[0]) +
@@ -112,10 +122,6 @@ function runClock() {
 	// timer[2] is number of 100th seconds.
 
 	timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
-
-	if (timer[3] > 12607000) {
-		gameOver();
-	}
 }
 
 function showMoves() {
@@ -123,93 +129,103 @@ function showMoves() {
 	updateStars();
 }
 
-function endGame() {
-	stopTimer();
-	window.alert(" You won!");
-}
-
-function gameOver() {
-	stopTimer();
-	window.alert(" Oh. Sorry - you're out of time. Try again");
-	resetGame();
-}
-
 function addListeners() {
-
-
-	DECK.addEventListener("click", function (e) {
-		console.log("Click:", e.target);
-		if (e.target && e.target.classList == "front") {
+	DECK.addEventListener("click", function(e) {
+		e.preventDefault();
+		// if (e.target && e.target.classList !== "front") {
+		// 	console.log("Click Other Target:", e.target);
+		// }
+		// if our target is front and we have less than 2 cards flipped.
+		if (
+			// e.target &&
+			e.target.classList == "front" &&
+			cardsFlipped.length < 2
+		) {
+			// first click of the game - start the timer.
 			if (firstClick) {
 				startTimer();
 				firstClick = false;
 			}
+			// set card to be target parent.
 			card = e.target.parentElement;
-			console.log("Click:", card);
+			//console.log("Click Parent:", card);
 
-			if (!card.classList.contains("match") && cardsOpen.length < 2) {
-				if (!card.classList.contains("open", "show")) {
-					cardsOpen.push(card);
-					card.classList.add("open", "show");
+			if (!card.classList.contains("match") && cardsFlipped.length < 2) {
+				if (!card.classList.contains("flipped")) {
+					cardsFlipped.push(card);
+					card.classList.add("flipped");
 				}
 			}
-
-			console.log("Open Cards: ", cardsOpen.length);
-
-			if (cardsOpen.length == 2) {
+			// Ok - we have two cards flipped - so let's check for a match.
+			if (cardsFlipped.length == 2) {
+				// increment moves and show them.
 				moves++;
-				//	showMoves();
+				showMoves();
 
-				if (cardsOpen[0].getAttribute("data-card") == cardsOpen[1].getAttribute("data-card")) {
-					cardsOpen[0].classList.add("match");
-					cardsOpen[1].classList.add("match");
+				if (
+					cardsFlipped[0].getAttribute("data-card") ==
+					cardsFlipped[1].getAttribute("data-card")
+				) {
+					// cards match so add match class.
+					cardsFlipped[0].classList.add("match");
+					cardsFlipped[1].classList.add("match");
+					setTimeout(function() {
+						cardsFlipped[0].classList.add("animated", "rubberBand");
+						cardsFlipped[1].classList.add("animated", "rubberBand");
+					}, 100);
+					// Keep track of number of cards matched. 16 is a win.
 					cardsMatched = cardsMatched + 2;
-					console.log("Cards Open:", cardsOpen);
-					setTimeout(function () {
-						cardsOpen[0].classList.remove("open", "show");
-						cardsOpen[1].classList.remove("open", "show");
-						cardsOpen = [];
+
+					setTimeout(function() {
+						cardsFlipped[0].classList.remove(
+							"flipped",
+							"animated",
+							"rubberBand"
+						);
+						cardsFlipped[1].classList.remove(
+							"flipped",
+							"animated",
+							"rubberBand"
+						);
+						cardsFlipped = [];
 						if (cardsMatched == 16) {
 							// Winner!
 							endGame();
 						}
 					}, 1000);
-
 				} else {
-					cardsOpen[0].classList.add("mismatched");
-					cardsOpen[1].classList.add("mismatched");
-					setTimeout(function () {
-						console.log("Cards Open:", cardsOpen);
-						cardsOpen[0].classList.add("animated", "wobble");
-						cardsOpen[1].classList.add("animated", "wobble");
-					}, 1000);
+					// not a match
+					cardsFlipped[0].classList.add("mismatched");
+					cardsFlipped[1].classList.add("mismatched");
+					setTimeout(function() {
+						cardsFlipped[0].classList.add("animated", "wobble");
+						cardsFlipped[1].classList.add("animated", "wobble");
+					}, 100);
 
-					setTimeout(function () {
-						console.log("Cards Open:", cardsOpen);
-						if (cardsOpen.length == 2) {
-							cardsOpen[0].classList.remove(
-								"open",
-								"show",
+					setTimeout(function() {
+						if (cardsFlipped.length == 2) {
+							cardsFlipped[0].classList.remove(
+								"flipped",
 								"animated",
 								"wobble",
 								"mismatched"
 							);
-							cardsOpen[1].classList.remove(
-								"open",
-								"show",
+							cardsFlipped[1].classList.remove(
+								"flipped",
 								"animated",
 								"wobble",
 								"mismatched"
 							);
-							cardsOpen = [];
+							cardsFlipped = [];
 						}
-					}, 2000);
+					}, 1000);
 				}
 			}
 		}
 	});
 }
 
+// Timer functions
 function startTimer() {
 	// Run this 100 times per second
 	clockID = setInterval(runClock, 10);
@@ -219,13 +235,14 @@ function stopTimer() {
 	timerRunning = false;
 	clearInterval(clockID);
 	actualTime = CLOCK.innerHTML;
-	console.log(actualTime);
 }
 
 function resetTimer() {
 	time = 0;
 	CLOCK.innerHTML = "00:00:00";
 }
+
+// Game functions
 
 function resetGame() {
 	// reset cards, score and timer.
@@ -234,10 +251,11 @@ function resetGame() {
 	moves = 0;
 	timer = [0, 0, 0, 0];
 	timerRunning = false;
-	cardsOpen = [];
+	cardsFlipped = [];
 	cardsMatched = 0;
 	firstClick = true;
-	console.log("Reset running");
+	DECK.classList.remove("hide");
+	SUCCESS.classList.remove("show");
 	showMoves();
 	loadCards();
 	addListeners();
@@ -259,28 +277,64 @@ function shuffle(array) {
 
 	return array;
 }
+// Card functions
+function generateCard(card) {
+	return `<li data-card="${card}" class="card"><div class="front"></div><div class="back"><i class="fa fa-${card}"></div></i></li>`;
+}
 
 function loadCards() {
 	var cardsHTML = shuffle(
-		CARDSET.map(function (card) {
+		CARDSET.map(function(card) {
 			return generateCard(card);
 		})
 	);
 
 	DECK.innerHTML = cardsHTML.join(" ");
+
+	cards = DECK.querySelectorAll(".card");
+	cards.forEach(function(card) {
+		card.classList.add("animated", "bounceInDown");
+	});
+	setTimeout(function() {
+		cards.forEach(function(card) {
+			card.classList.remove("animated", "bounceInDown");
+		});
+	}, 2000);
+}
+
+function resetStars() {
+	for (var i = 0; i < 5; i++) {
+		STARSET[i].classList.remove("hide");
+	}
+}
+
+function endGame() {
+	stopTimer();
+	FINAL.innerHTML = `<span>Time elapsed ${actualTime}</span>
+	<span>in ${moves} Moves and earning ${currentStars} Stars</span>`;
+	DECK.classList.add("hide");
+	PLAYAGAIN.classList.toggle("hide");
+	PLAYAGAIN.classList.add("animated", "fadeInUp");
+	SUCCESS.classList.add("show");
+	playAgain = setTimeout(function() {
+		PLAYAGAIN.classList.toggle("hide");
+	}, 2000);
+	playAgain = setTimeout(function() {
+		PLAYAGAIN.classList.remove("animated", "fadeInUp");
+	}, 4000);
 }
 
 // Initialize Game
 function initGame() {
 	// load game to start over.
 
-	RESTART.addEventListener("click", function (e) {
-		console.log("Reset pressed");
-
+	RESTART.addEventListener("click", function(e) {
 		resetGame();
 	});
-
-	loadCards();
+	PLAYAGAIN.addEventListener("click", function(e) {
+		resetGame();
+	});
+	resetGame();
 	addListeners();
 }
 
@@ -289,9 +343,9 @@ initGame();
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "flipped" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do match, lock the cards in the flipped position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
